29ec7a53c02873d15d18ff04c79397e9af5ac003
valerio ferreira <valdemito16@gmail.com>
2025-04-25 19:26:18 -0300

    Mundando rota da base vite config

--- projeto 2/vite.config.js
+++ projeto 2/vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:"/viscosidade",
+  base:"/",
   build: {
     outDir: 'dist',
     assetsDir: 'assets' // isso deve corresponder ao caminho dos erros
