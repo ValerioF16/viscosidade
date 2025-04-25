dda147c82a0cace5192d9accac143523075bc01e
valerio ferreira <valdemito16@gmail.com>
2025-04-25 19:20:00 -0300

    Arrumando config do vite

--- projeto 2/vite.config.js
+++ projeto 2/vite.config.js
@@ -4,5 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:"/viscosidade"
+  base:"/viscosidade",
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets' // isso deve corresponder ao caminho dos erros
+  }
 })
